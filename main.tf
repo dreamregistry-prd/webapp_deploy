95debd070641ceebeca512c67732ba56ac75042a
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-05-23 11:29:21 +0200

    fix set access

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "aws_security_group_rule" "allow_http" {
   from_port                = 80
   to_port                  = 80
   protocol                 = "tcp"
-  source_security_group_id = data.aws_lb.lb.security_groups[0]
+  source_security_group_id = tolist(data.aws_lb.lb.security_groups)[0]
 }
 
 resource "aws_security_group_rule" "allow_all_out" {
