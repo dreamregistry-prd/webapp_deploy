80ea14b7cfa12739b00ee2140e23865297293a44
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-05-23 11:15:53 +0200

    configure envoy proxy deployment

--- main.tf
+++ main.tf
@@ -16,6 +16,144 @@ terraform {
 provider "random" {}
 provider "aws" {}
 
+data "aws_ecs_cluster" "fargate" {
+  cluster_name = var.ecs_cluster_name
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  tags = {
+    Tier = "private"
+  }
+}
+
+data "aws_lb" "lb" {
+  arn = var.alb_arn
+}
+
+data "aws_lb_listener" "https" {
+  load_balancer_arn = data.aws_lb.lb.arn
+  port              = 443
+}
+
+data "aws_route53_zone" "root" {
+  name = var.domain_suffix
+}
+
+locals {
+  project_name  = var.project_name != null ? var.project_name : basename(var.dream_project_dir)
+  domain_prefix = var.domain_prefix != null ? var.domain_prefix : local.project_name
+  domain_name   = "${local.domain_prefix}.${var.domain_suffix}"
+}
+
+resource "aws_ecs_service" "nginx" {
+  name            = local.project_name
+  cluster         = data.aws_ecs_cluster.fargate.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.private.ids
+    security_groups  = [aws_security_group.web.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "envoy"
+    container_port   = 80
+  }
+}
+
+resource "random_pet" "task_definition_name" {
+  prefix = local.project_name
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = random_pet.task_definition_name.id
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  container_definitions    = jsonencode([
+    {
+      name         = "envoy"
+      image        = "public.ecr.aws/bitnami/envoy:1.26.1"
+      cpu          = 256
+      memory       = 512
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 80
+          protocol      = "tcp"
+        },
+      ]
+    },
+  ])
+}
+
+resource "aws_lb_target_group" "app" {
+  name_prefix = local.project_name
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+module "alb_certificate" {
+  source             = "github.com/hereya/terraform-modules//alb-certificate/module?ref=v0.18.0"
+  alb_arn            = var.alb_arn
+  domain_name_prefix = local.domain_prefix
+  route53_zone_name  = var.domain_suffix
+}
+
+resource "aws_lb_listener_rule" "host_based_weighted_routing" {
+  listener_arn = data.aws_lb_listener.https.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.domain_name]
+    }
+  }
+}
+
+resource "aws_security_group" "web" {
+  name = "web"
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  security_group_id        = aws_security_group.web.id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_lb.lb.security_groups[0]
+}
+
+resource "aws_security_group_rule" "allow_all_out" {
+  security_group_id = aws_security_group.web.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
 output "DEPLOYED_IMAGE" {
   value = var.dream_env.DOCKER_IMAGES[0]
--- variables.tf
+++ variables.tf
@@ -8,6 +8,31 @@ variable "cognito_user_pool_domain" {
   description = "The fully-qualified domain name of the user pool"
 }
 
+variable "ecs_cluster_name" {
+  type        = string
+  description = "The name of the ECS cluster to deploy the app to"
+}
+
+variable "alb_arn" {
+  type        = string
+  description = "The ARN of the ALB to expose the app on"
+}
+
+variable "alb_https_listener_arn" {
+  type        = string
+  description = "The ARN of the HTTPS listener on the ALB"
+}
+
+variable "domain_prefix" {
+  description = "domain prefix to use for the service"
+  type        = string
+  default     = null
+}
+
+variable "domain_suffix" {
+  description = "domain suffix to use for the service"
+  type        = string
+}
 
 variable "dream_env" {
   description = "dream app environment variables to set"
@@ -19,4 +44,15 @@ variable "dream_secrets" {
   description = "dream app secrets to set"
   type        = set(string)
   default     = []
+}
+
+variable "dream_project_dir" {
+  description = "root directory of the project sources"
+  type        = string
+}
+
+variable "project_name" {
+  description = "name of the project"
+  type        = string
+  default     = null
 }
